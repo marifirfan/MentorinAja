297b1d812468a342e3f5782829c3426cbe80c6c3
krisnabimantoro <krisnabmntr@gmail.com>
2024-06-14 10:29:55 +0700

    update login

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,6 @@
         <?php
         $conn = oci_connect('mentorinaja', 'krisna2019', '//localhost/XE');
-        // Check connection
+      
         if (!$conn) {
             echo 'Failed to connect to oracle' . "<br>";
             
@@ -12,37 +12,5 @@
 
         
 
-        // //query to fetch data
-        // $query = 'SELECT Movie_id, Title FROM MOVIE';
-        // $stid = oci_parse($conn, $query);
-
-        // if (!$stid) {
-        //     $m = oci_error($conn);
-        //     trigger_error('Could not parse statement: '. $m['message'], E_USER_ERROR);
-        // }
-        // print "oci_parse executed";
-        // echo '<br>';
-        
-        // $r = oci_execute($stid);
-        // if (!$r) {
-        //     $m = oci_error($stid);
-        //     trigger_error('Could not execute statement: '. $m['message'], E_USER_ERROR);
-        // }
-        // print "oci executed". "\n";
-        // echo '<br>';
-
-        // //retrieving data as a tuple 
-        // print '<table border="1">';
-        // while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-        //     print '<tr>';
-        //     foreach ($row as $item) {
-        //         print '<td>'.($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp').'</td>';
-        //     }
-        //     print '</tr>';
-        // }
-
-        // print '</table>';
-        //         print "table end";
-        // oci_close($conn);
 
         ?>
--- php/login.php
+++ php/login.php
@@ -21,42 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($user) {
             echo "Login berhasil!<br>";
         } else {
-            // Jika pengguna tidak ditemukan, tampilkan pesan error
             echo "Login gagal! Email atau password salah.";
         }
 
-        // if ($user && password_verify($password, $user['PASSWORD'])) {
-        //     $_SESSION['user_id'] = $user['ID_USER'];
-        //     $_SESSION['user_name'] = $user['NAME'];
-        //     oci_free_statement($stid);
-        //     oci_close($conn);
-        //     header("Location: welcome.php");
-        //     exit();
-        // } else {
-        //     echo "Invalid email or password.";
-        // }
         oci_free_statement($stid);
     }
 }
-// oci_close($conn);
-// function checkLogin($email, $password) {
-//     global $conn;
-
-//     $sql = 'SELECT ID_USER, NAME, EMAIL FROM TB_USER WHERE EMAIL = :email AND PASSWORD = :password';
-//     $stid = oci_parse($conn, $sql);
-//     oci_bind_by_name($stid, ':email', $email);
-//     oci_bind_by_name($stid, ':password', $password);
-    
-//     oci_execute($stid);
-
-//     $row = oci_fetch_assoc($stid);
-//     oci_free_statement($stid);
-
-//     if ($row) {
-//         return $row;
-//     } else {
-//         return false;
-//     }
-// }
-
 ?>
